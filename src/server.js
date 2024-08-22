5f8730e9c2049a253627f2da92d3091a04deb20a
ing-lp-tech <ing.lp.tech@gmail.com>
2024-08-21 21:07:10 -0300

    feat:add .env and modify server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,29 @@
 import express from "express";
 import cors from "cors";
 import admin from "firebase-admin";
+import dotenv from "dotenv";
 
-// Configuración de Firebase Admin
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
+
+// Configurar Firebase utilizando las variables de entorno
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"), // Reemplazar saltos de línea
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
+
 admin.initializeApp({
-  credential: admin.credential.cert("./firebase-adminsdk.json"),
-  databaseURL: "https://iot-patty-default-rtdb.firebaseio.com/",
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://your-project-id.firebaseio.com/",
 });
 
 const db = admin.database();
@@ -19,17 +37,29 @@ app.use(express.json());
 app.post("/api/led/:state", (req, res) => {
   const state = req.params.state;
   ref
-    .set(state) // Actualiza el estado en la base de datos
-    .then(() => res.status(200).send(`LED is turned ${state}`))
-    .catch((error) => res.status(500).send(error.message));
+    .set(state)
+    .then(() => {
+      console.log(`LED set to ${state}`);
+      res.status(200).send(`LED is turned ${state}`);
+    })
+    .catch((error) => {
+      console.error("Error setting LED state:", error);
+      res.status(500).send(error.message);
+    });
 });
 
 // Endpoint para obtener el estado actual del LED
 app.get("/api/led/status", (req, res) => {
   ref
     .once("value")
-    .then((snapshot) => res.send(snapshot.val()))
-    .catch((error) => res.status(500).send(error.message));
+    .then((snapshot) => {
+      console.log("LED status retrieved:", snapshot.val());
+      res.send(snapshot.val());
+    })
+    .catch((error) => {
+      console.error("Error getting LED status:", error);
+      res.status(500).send(error.message);
+    });
 });
 
 const PORT = process.env.PORT || 10000;
